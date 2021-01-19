5861309aa8e5772bca3e91b21f455ef652d542fe
Benjamin <benjamin.nese@gmail.com>
2021-01-19 16:58:15 +0100

    changed greetings

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) =>{
-    res.send({hi: 'there'});
+    res.send({hi: 'Movie Selector'});
 });
 
 //Port fra heroku eller 5000 om lokalt
